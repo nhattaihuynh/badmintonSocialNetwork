877ad14299080d10fa4aa0b81f347f51ee03d49d
taihuynh <nhattaihuynh@gmail.com>
2025-04-15 00:21:13 +0700

    init config-server

---
+++ services/config-server/src/test/java/com/social/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
