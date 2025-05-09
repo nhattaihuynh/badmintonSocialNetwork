9470bee54544bc43dc3a9361f374a05d996afcd9
taihuynh <nhattaihuynh@gmail.com>
2025-05-09 19:44:40 +0700

    new service main-contract-service

---
+++ services/main-contract-service/src/main/java/com/social/main_contract_service/MainContractServiceApplication.java
@@ -0,0 +1,13 @@
+package com.social.main_contract_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainContractServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainContractServiceApplication.class, args);
+	}
+
+}
