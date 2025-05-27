a9ab149ef0726822c6c1b2f4f3ed0f9f8f2dbead
taihuynh <nhattaihuynh@gmail.com>
2025-05-27 22:03:12 +0700

    init friendship-service

---
+++ services/friendship-service/src/test/kotlin/com/social/friendship_service/FriendshipServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.social.friendship_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FriendshipServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
