c3952cdafbd24cf9b575f81743ef9da8a26cf100
taihuynh <nhattaihuynh@gmail.com>
2025-10-26 20:51:15 +0700

    api create comment + api get all comments

--- services/comment-service/src/main/java/com/social/comment_service/controller/CommentController.java
+++ services/comment-service/src/main/java/com/social/comment_service/controller/CommentController.java
@@ -1,10 +1,29 @@
 package com.social.comment_service.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.social.comment_service.dto.request.CommentCreateRequest;
+import com.social.comment_service.dto.response.CommentResponseDto;
+import com.social.comment_service.service.CommentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
+    private final CommentService commentService;
 
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public CommentResponseDto createComment(@RequestBody CommentCreateRequest request) {
+        return commentService.createComment(request);
+    }
+
+    @GetMapping("/{postId}/comments-with-replies")
+    public List<CommentResponseDto> getAllCommentsIncludeReplies(@PathVariable("postId") UUID postId) {
+        return commentService.getAllCommentsIncludeReplies(postId);
+    }
 }
--- services/comment-service/src/main/java/com/social/comment_service/service/CommentService.java
+++ services/comment-service/src/main/java/com/social/comment_service/service/CommentService.java
@@ -1,7 +1,70 @@
 package com.social.comment_service.service;
 
+import com.social.comment_service.dto.request.CommentCreateRequest;
+import com.social.comment_service.dto.response.CommentResponseDto;
+import com.social.comment_service.entity.Comment;
+import com.social.comment_service.repository.CommentRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class CommentService {
+    private final CommentRepository repository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.repository = commentRepository;
+    }
+
+    @Transactional
+    public CommentResponseDto createComment(CommentCreateRequest request) {
+        Comment comment = Comment.builder()
+                .postId(request.postId())
+                .profileId(request.profileId())
+                .parentCommentId(request.parentCommentId())
+                .content(request.content())
+                .build();
+        repository.save(comment);
+        return new CommentResponseDto(
+                comment.getId(),
+                comment.getPostId(),
+                comment.getProfileId(),
+                comment.getParentCommentId(),
+                comment.getContent(),
+                comment.getCreatedAt(),
+                comment.getUpdatedAt(),
+                null
+        );
+    }
+
+    @Transactional
+    public List<CommentResponseDto> getAllCommentsIncludeReplies(UUID postId) {
+        List<Comment> comments = repository.findByPostIdOrderByCreatedAtDesc(postId);
+        // Map comments to CommentResponseDto with nested replies
+        Map<UUID, CommentResponseDto> commentsResponse = new HashMap<>();
+        for (Comment comment : comments) {
+            if (comment.getParentCommentId() == null) {
+                commentsResponse.put(comment.getId(), toDto(comment));
+            } else {
+                CommentResponseDto parentComment = commentsResponse.get(comment.getParentCommentId());
+                parentComment.replyComments().add(toDto(comment));
+            }
+        }
+
+        return commentsResponse.values().stream().toList();
+    }
+
+    private CommentResponseDto toDto(Comment comment) {
+        return new CommentResponseDto(
+                comment.getId(),
+                comment.getPostId(),
+                comment.getProfileId(),
+                comment.getParentCommentId(),
+                comment.getContent(),
+                comment.getCreatedAt(),
+                comment.getUpdatedAt(),
+                List.of()
+        );
+    }
 }
