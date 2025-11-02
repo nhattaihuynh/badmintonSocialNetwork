ddf58b58f98f5d1038ad4e0ece942fa500245ec6
taihuynh <nhattaihuynh@gmail.com>
2025-11-02 23:39:51 +0700

    init media-upload-service

---
+++ services/media-upload-service/src/main/kotlin/com/social/media_upload_service/MediaUploadServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.social.media_upload_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MediaUploadServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MediaUploadServiceApplication>(*args)
+}
